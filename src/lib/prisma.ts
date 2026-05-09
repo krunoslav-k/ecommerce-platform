4b1a70a6d3a738421159e1d83b2862ca942356be
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-05-09 15:17:02 +0200

    fix: fix prisma initialization and sqlite table issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,21 @@
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
 export const db =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ['query'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db;
+}
