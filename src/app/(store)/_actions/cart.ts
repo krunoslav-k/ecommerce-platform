78d1d9153f32800cb92238d4fa87c9458867931f
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-05-18 17:19:59 +0200

    feat(store/cart): implement delete item and update item quantity

--- src/app/(store)/_actions/cart.ts
+++ src/app/(store)/_actions/cart.ts
@@ -2,12 +2,11 @@
 
 import { db } from '@/db/prisma';
 import { getCurrentUser } from '@/lib/auth';
-import { getOrCreateCart } from '@/lib/cart';
+import { getCart, getOrCreateCart } from '@/lib/cart';
 import { revalidatePath } from 'next/cache';
 
 export async function addToCart(productId: string) {
   const userId = await getCurrentUser();
-
   const cart = await getOrCreateCart(userId);
 
   const existingItem = await db.cartItem.findUnique({
@@ -36,6 +35,58 @@ export async function addToCart(productId: string) {
         cartId: cart.id,
         productId,
       },
+    });
+  }
+
+  revalidatePath('/cart');
+}
+
+export async function deleteItemFromCart(productId: string) {
+  const userId = await getCurrentUser();
+  const cart = await getCart(userId);
+
+  if (!cart) return;
+
+  const existingItem = await db.cartItem.findUnique({
+    where: {
+      cartId_productId: {
+        cartId: cart.id,
+        productId,
+      },
+    },
+  });
+
+  if (existingItem) {
+    await db.cartItem.delete({
+      where: {
+        cartId_productId: {
+          cartId: cart.id,
+          productId,
+        },
+      },
+    });
+
+    revalidatePath('/cart');
+  }
+}
+
+export async function updateCartItemQuantity(
+  productId: string,
+  quantity: number
+) {
+  const userId = await getCurrentUser();
+  const cart = await getCart(userId);
+
+  if (!cart) return;
+
+  if (quantity <= 0) {
+    await db.cartItem.delete({
+      where: { cartId_productId: { cartId: cart.id, productId } },
+    });
+  } else {
+    await db.cartItem.update({
+      where: { cartId_productId: { cartId: cart.id, productId } },
+      data: { quantity },
     });
   }
 
--- src/app/(store)/cart/page.tsx
+++ src/app/(store)/cart/page.tsx
@@ -4,8 +4,10 @@ import { Input } from '@/components/ui/input';
 import { getCurrentUser } from '@/lib/auth';
 import { getCart } from '@/lib/cart';
 import { formatCurrency } from '@/lib/formatters';
-import { ShoppingBag, X } from 'lucide-react';
+import { Minus, Plus, ShoppingBag, X } from 'lucide-react';
 import Image from 'next/image';
+import { deleteItemFromCart, updateCartItemQuantity } from '../_actions/cart';
+import Link from 'next/link';
 
 export default async function CartPage() {
   const userId = await getCurrentUser();
@@ -32,21 +34,18 @@ export default async function CartPage() {
                 <div className="flex h-full max-h-30 items-center justify-center md:col-span-1 md:col-start-1 md:row-span-2 md:max-h-25 md:self-center">
                   <Image
                     src={item.product.images[0].url}
-                    alt="iphone 17"
+                    alt={item.product.name}
                     className="max-h-full max-w-full object-contain"
                     height={100}
                     width={100}
                   />
                 </div>
-
                 <div className="text-lg font-bold md:col-span-2 md:col-start-2 md:text-base">
                   {item.product.name}
                 </div>
-
                 <div className="wrap-break-words line-clamp-3 w-full min-w-0 pr-2 text-sm text-gray-500 md:col-span-2 md:col-start-2">
                   {item.product.description}
                 </div>
-
                 <div className="mt-2 flex items-center justify-between border-t pt-3 md:mt-0 md:contents md:border-0 md:pt-0">
                   <div className="md:col-start-4">
                     <span className="block text-xs text-gray-400">Each</span>
@@ -59,7 +58,10 @@ export default async function CartPage() {
                     <span className="block text-xs text-gray-400">
                       Quantity
                     </span>
-                    <span>{item.quantity}</span>
+                    <QuantityPicker
+                      quantity={item.quantity}
+                      productId={item.productId}
+                    />
                   </div>
 
                   <div className="md:col-start-6">
@@ -71,68 +73,112 @@ export default async function CartPage() {
                     </span>
                   </div>
                 </div>
-
-                <div className="cursor-pointer justify-self-end p-2 text-center md:col-start-7 md:px-2">
-                  <Button size="icon-sm" variant="destructive">
-                    <X />
-                  </Button>
-                </div>
+                <RemoveFromCartButton productId={item.productId} />
               </div>
             );
           })}
         </div>
 
-        <div className="flex h-fit w-full flex-col items-center gap-4 rounded-lg bg-zinc-50 px-6 py-3.5 text-sm lg:w-1/3">
-          <h2 className="mb-4 self-start text-lg font-bold">Summary</h2>
-
-          <div className="border-input bg-background focus-within:ring-ring borderfocus-within:ring-1 flex h-10 w-full max-w-sm items-center overflow-hidden rounded-md">
-            <Input
-              type="text"
-              placeholder="Promo code"
-              className="h-full border-0 bg-transparent shadow-none focus-visible:ring-0 focus-visible:ring-offset-0"
-            />
-            <Button className="h-full rounded-l-none bg-gray-600 text-sm hover:bg-gray-700">
-              Submit
-            </Button>
-          </div>
-
-          <div className="flex w-full items-center justify-between">
-            <div>Shipping cost</div>
-            <div>3,99€</div>
-          </div>
-
-          <div className="flex w-full items-center justify-between">
-            <div>Tax</div>
-            <div>{formatCurrency((0.25 * subtotalInCents) / 100)}</div>
-          </div>
-
-          <div className="flex w-full items-center justify-between">
-            <div>Subtotal</div>
-            <div>{formatCurrency(subtotalInCents / 100)}</div>
-          </div>
-
-          <div className="flex w-full items-center justify-between">
-            <div>Discount</div>
-            <div>- 0€</div>
-          </div>
-
-          <hr className="border-0.25 w-full border-gray-200" />
-
-          <div className="flex w-full items-center justify-between">
-            <div>Total</div>
-            <div className="font-bold">
-              {formatCurrency(subtotalInCents / 100 + 3.99)}
-            </div>
-          </div>
-
-          <Button
-            size="lg"
-            className="my-4 w-full bg-sky-700 text-white hover:bg-sky-800"
-          >
-            <ShoppingBag /> Checkout
-          </Button>
-        </div>
+        <TotalSummaryCard subtotalInCents={subtotalInCents} />
       </div>
     </>
+  );
+}
+
+function QuantityPicker({
+  quantity,
+  productId,
+}: {
+  quantity: number;
+  productId: string;
+}) {
+  return (
+    <div className="flex w-fit items-center overflow-hidden rounded-md border border-slate-200">
+      <form action={updateCartItemQuantity.bind(null, productId, quantity - 1)}>
+        <Button className="flex h-7 w-7 items-center justify-center bg-white transition-colors hover:bg-slate-50">
+          <Minus className="h-3.5 w-3.5 text-slate-600" />
+        </Button>
+      </form>
+
+      <div className="flex h-7 w-8 items-center justify-center border-x border-slate-200 bg-slate-50 text-sm font-medium text-slate-900">
+        {quantity}
+      </div>
+
+      <form action={updateCartItemQuantity.bind(null, productId, quantity + 1)}>
+        <Button className="flex h-7 w-7 items-center justify-center bg-white transition-colors hover:bg-slate-50">
+          <Plus className="h-3.5 w-3.5 text-slate-600" />
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+function RemoveFromCartButton({ productId }: { productId: string }) {
+  return (
+    <div className="cursor-pointer justify-self-end p-2 text-center md:col-start-7 md:px-2">
+      <form action={deleteItemFromCart.bind(null, productId)}>
+        <Button type="submit" size="icon-sm" variant="destructive">
+          <X />
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+function TotalSummaryCard({ subtotalInCents }: { subtotalInCents: number }) {
+  return (
+    <div className="flex h-fit w-full flex-col items-center gap-4 rounded-lg bg-zinc-50 px-6 py-3.5 text-sm lg:w-1/3">
+      <h2 className="mb-4 self-start text-lg font-bold">Summary</h2>
+
+      <div className="border-input bg-background focus-within:ring-ring borderfocus-within:ring-1 flex h-10 w-full max-w-sm items-center overflow-hidden rounded-md">
+        <Input
+          type="text"
+          placeholder="Promo code"
+          className="h-full border-0 bg-transparent shadow-none focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+        <Button className="h-full rounded-l-none bg-gray-600 text-sm hover:bg-gray-700">
+          Submit
+        </Button>
+      </div>
+
+      <div className="flex w-full items-center justify-between">
+        <div>Shipping cost</div>
+        <div>3,99€</div>
+      </div>
+
+      <div className="flex w-full items-center justify-between">
+        <div>Tax</div>
+        <div>{formatCurrency((0.25 * subtotalInCents) / 100)}</div>
+      </div>
+
+      <div className="flex w-full items-center justify-between">
+        <div>Subtotal</div>
+        <div>{formatCurrency(subtotalInCents / 100)}</div>
+      </div>
+
+      <div className="flex w-full items-center justify-between">
+        <div>Discount</div>
+        <div>- 0€</div>
+      </div>
+
+      <hr className="border-0.25 w-full border-gray-200" />
+
+      <div className="flex w-full items-center justify-between">
+        <div>Total</div>
+        <div className="font-bold">
+          {formatCurrency(subtotalInCents / 100 + 3.99)}
+        </div>
+      </div>
+
+      <Button
+        size="lg"
+        className="my-4 w-full bg-sky-700 text-white hover:bg-sky-800"
+        asChild
+      >
+        <Link href="/checkout">
+          <ShoppingBag /> Checkout
+        </Link>
+      </Button>
+    </div>
   );
 }
